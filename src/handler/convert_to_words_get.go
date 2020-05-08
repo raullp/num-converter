ad09670d788e5e03317a417bd1d1ba02c5d208a0
Raul Lopez <raul.lopez@nexttrucking.com>
2020-05-08 08:05:29 -0400

    Raise error properly in convert words handler

--- src/handler/convert_to_words_get.go
+++ src/handler/convert_to_words_get.go
@@ -1,22 +1,26 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
 
+	"../error"
 	"../numconv"
 	"github.com/gin-gonic/gin"
 )
 
 func ConvertToWords(c *gin.Context) {
+	const op error.Op = "convert.ConvertToWords"
+	const badRequest error.Kind = error.KindBadRequest
 	number, err := strconv.Atoi(c.Param("number"))
 	if err != nil {
-		c.Error(err) // TODO: wrap this error as an Invalid Parameter
+		c.Error(error.E(errors.New("number: is not a valid number"), op, badRequest))
 		return
 	}
 	words := numconv.ConvertToWords(number)
 	c.JSON(http.StatusOK, gin.H{
-		"status":         "success",
+		"status":         "ok",
 		"num_in_english": words,
 	})
 }
--- src/main_test.go
+++ src/main_test.go
@@ -12,6 +12,7 @@ import (
 type Response struct {
 	NumInEnglish string `json:"num_in_english"`
 	Status       string `json:"status"`
+	Detail       string `json:"detail"`
 }
 
 func TestConvertNumberToWords(t *testing.T) {
@@ -27,6 +28,21 @@ func TestConvertNumberToWords(t *testing.T) {
 	json.Unmarshal([]byte(w.Body.String()), &response)
 
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "success", response.Status)
+	assert.Equal(t, "ok", response.Status)
 	assert.Equal(t, words, response.NumInEnglish)
+}
+
+func TestConvertNumberToWordsReturnsBadRequestWhenNonNumber(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/converter/number-to-words/a1234b", nil)
+	router.ServeHTTP(w, req)
+
+	response := Response{}
+	json.Unmarshal([]byte(w.Body.String()), &response)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, "error", response.Status)
+	assert.Equal(t, "number: is not a valid number", response.Detail)
 }
